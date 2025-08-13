0399888ff23ee5a1df741afadee4fcd5f7b7d199
saurabhayush959-ux <saurabhayush959@gmail.com>
2025-08-13 02:11:23 -0700

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,15 @@
+
+def greet_user():
+    
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    
+    
+    full_name = first_name + " " + last_name
+    
+   
+    print(f"Hello, {full_name}! Welcome!")
+
+
+if __name__ == "__main__":
+    greet_user()
